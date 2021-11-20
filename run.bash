abcd02b051726eb54f72c5440ceccad104a31b7b
Paul Tarvydas <guitarvydas@gmail.com>
2021-11-20 10:05:28 -0500

    use pfr instead of pfrs

--- run.bash
+++ run.bash
@@ -9,10 +9,10 @@
 # ./dpf  test3.txt test3.ohm support.js
 
 
-pfrs test.txt test.ohm test.action support.js
-pfrs test2.txt test2.ohm test2.action support.js
-pfrs test2.txt test2.ohm test2a.action support.js
-pfrs test3.txt test4.ohm test4.action support.js
+pfr test.txt test.ohm test.action support.js
+pfr test2.txt test2.ohm test2.action support.js
+pfr test2.txt test2.ohm test2a.action support.js
+pfr test3.txt test4.ohm test4.action support.js
 
 echo
 echo '*** pf ***'
